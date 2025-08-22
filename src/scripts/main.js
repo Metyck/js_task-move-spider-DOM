1c854e94f76e252ff0a85417358e091938cefa0b
Matvey Turluk <matychencia.ua@gmail.com>
2025-08-22 12:51:56 +0300

    fixed cases of srolling or approximation page, ensured the moving left in the case of move < 0 to be 0, eliminated the conflict with Y axis to be orientated directly by the wall's heigth, not by the width

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,42 +3,46 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-const wallBounding = wall.getBoundingClientRect();
-
 document.addEventListener('click', (e) => {
   // write code here
   const target = e.target.closest('.wall');
+  const wallBounding = wall.getBoundingClientRect();
 
   if (target) {
     const x = e.clientX;
     const y = e.clientY;
+    // current cursor position
 
     const wallWidth = parseFloat(getComputedStyle(wall).width);
     const wallHeight = parseFloat(getComputedStyle(wall).height);
     const leftBorder = parseFloat(getComputedStyle(wall).borderLeftWidth);
     const topBorder = parseFloat(getComputedStyle(wall).borderTopWidth);
+    // wall sizes
 
     const moveX = x - wallBounding.x - leftBorder - spider.offsetWidth / 2;
     const moveY = y - wallBounding.y - topBorder - spider.offsetHeight / 2;
+    // default axises moving
 
     const moveXmax = wallWidth - spider.offsetWidth;
-    const moveYmax = wallWidth - spider.offsetHeight;
+    const moveYmax = wallHeight - spider.offsetHeight;
+    // axises moving in case of clicking on the walls border
 
     if (moveX > wallWidth - spider.offsetWidth) {
       spider.style.left = `${moveXmax}px`;
     } else if (moveX < 0) {
-      spider.style.left = '';
+      spider.style.left = 0;
     } else {
       spider.style.left = `${moveX}px`;
     }
+    // X axis behavior
 
     if (moveY > wallHeight - spider.offsetHeight) {
       spider.style.top = `${moveYmax}px`;
     } else if (moveY < 0) {
-      spider.style.top = '';
+      spider.style.top = 0;
     } else {
       spider.style.top = `${moveY}px`;
     }
-  } else {
+    // Y axis behavior
   }
 });
